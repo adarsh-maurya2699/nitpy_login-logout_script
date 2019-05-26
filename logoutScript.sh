3885a1d567971d6da1e638571d1699d83e6164c6
adarsh-maurya2699 <47120729+adarsh-maurya2699@users.noreply.github.com>
2019-05-26 17:56:03 +0530

    Corrected logout.side path

--- logoutScript.sh
+++ logoutScript.sh
@@ -1,1 +1,1 @@
-selenium-side-runner -c "browserName=chrome chromeOptions.args=[disable-infobars, headless] --ignore-certificate-errors" /home/adarsh/Downloads/logout.side
+selenium-side-runner -c "browserName=chrome chromeOptions.args=[disable-infobars, headless] --ignore-certificate-errors" logout.side
