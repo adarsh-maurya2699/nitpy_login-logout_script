7b9b7b83d513e592309d7c0f21534066ec1a2f96
adarsh-maurya2699 <47120729+adarsh-maurya2699@users.noreply.github.com>
2019-05-26 17:54:04 +0530

    Corrected the login.side path

--- loginScript.sh
+++ loginScript.sh
@@ -1,1 +1,1 @@
-selenium-side-runner -c "browserName=chrome chromeOptions.args=[disable-infobars, headless] --ignore-certificate-errors" /home/adarsh/Downloads/login.side
+selenium-side-runner -c "browserName=chrome chromeOptions.args=[disable-infobars, headless] --ignore-certificate-errors" login.side
